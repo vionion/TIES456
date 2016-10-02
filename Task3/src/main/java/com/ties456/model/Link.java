a80c6ed520b11dbbbef5db4a5ae3228af078c638
chinhnk <chinhnkse02486@fpt.edu.vn>
2016-10-02 15:52:57 +0300

    new models

---
+++ Task3/src/main/java/com/ties456/model/Link.java
@@ -0,0 +1,31 @@
+package com.ties456.model;
+
+/**
+ * Created by chinhnk on 10/2/2016.
+ */
+public class Link {
+
+    private String href;
+    private String rel;
+
+    public Link(String href, String rel) {
+        this.href = href;
+        this.rel = rel;
+    }
+
+    public String getHref() {
+        return href;
+    }
+
+    public void setHref(String href) {
+        this.href = href;
+    }
+
+    public String getRel() {
+        return rel;
+    }
+
+    public void setRel(String rel) {
+        this.rel = rel;
+    }
+}
