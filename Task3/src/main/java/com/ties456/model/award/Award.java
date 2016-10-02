e2ed6e868964d7756094c4be4d84a5828c9df849
chinhnk <chinhnkse02486@fpt.edu.vn>
2016-10-02 22:33:30 +0300

    try again

--- Task3/src/main/java/com/ties456/model/award/Award.java
+++ Task3/src/main/java/com/ties456/model/award/Award.java
@@ -4,4 +4,32 @@ package com.ties456.model.award;
  * Created by chinhnk on 10/2/2016.
  */
 public class Award {
+
+    private int id;
+    private String name;
+    private int year;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
 }
