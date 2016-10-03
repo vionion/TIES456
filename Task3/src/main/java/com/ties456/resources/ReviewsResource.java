eb12dc00ea9aa0b0c3d0975ebb45f05380394937
chinhnk <chinhnkse02486@fpt.edu.vn>
2016-10-03 09:54:28 +0300

    - Restored methods in AwardResource and ReviewResource

--- Task3/src/main/java/com/ties456/resources/ReviewsResource.java
+++ Task3/src/main/java/com/ties456/resources/ReviewsResource.java
@@ -24,6 +24,64 @@ public class ReviewsResource {
 
     private ReviewService reviewService = new ReviewServiceImpl();
 
+//    @GET
+//    public List<Review> getAll() {
+//        return reviewService.getAll();
+//    }
+
+    @GET
+    @Path("/{id}")
+    public Review getOne(@PathParam("id") long id) {
+        Review result = reviewService.getById(id);
+        if (result == null) {
+            throw new WebApplicationException(Response.Status.NOT_FOUND);
+        } else {
+            return result;
+        }
+    }
+
+    @PUT
+    public ResponseEntity<Review> createReview(@RequestBody Review review) {
+        if (reviewService.isReviewExist(review.getId())) {
+            return new ResponseEntity<Review>(HttpStatus.CONFLICT);
+        }
+        Review result = reviewService.saveReview(review);
+        return new ResponseEntity<Review>(result, HttpStatus.CREATED);
+    }
+
+    @POST
+    @Path("/{id}")
+    public ResponseEntity<Review> updateReview(@PathParam("id") long id, @RequestBody Review reviewToUpdate) {
+        Review currentReview = reviewService.getById(id);
+        if (currentReview == null) {
+            return new ResponseEntity<Review>(HttpStatus.NOT_FOUND);
+        }
+        currentReview.setDetails(reviewToUpdate.getDetails());
+        currentReview.setRating(reviewToUpdate.getRating());
+        reviewService.updateReview(currentReview);
+        return new ResponseEntity<Review>(currentReview, HttpStatus.OK);
+    }
+
+    @DELETE
+    @Path("/{id}")
+    public ResponseEntity<Review> deleteReview(@PathParam("id") long id) {
+        if (id < 0) {
+            throw new MyNotFoundException("Dude, try to be more positive!");
+        }
+        Review review = reviewService.getById(id);
+        if (review == null) {
+            return new ResponseEntity<Review>(HttpStatus.NOT_FOUND);
+        }
+        reviewService.deleteReviewById(id);
+        return new ResponseEntity<Review>(HttpStatus.NO_CONTENT);
+    }
+
+    @DELETE
+    public ResponseEntity<Review> deleteAllReviews() {
+        reviewService.deleteAllReviews();
+        return new ResponseEntity<Review>(HttpStatus.NO_CONTENT);
+    }
+
     @GET
     public List<Review> getReviews(@PathParam(Constants.MOVIE_ID) long movieId) {
         return reviewService.getByMovieId(movieId);
